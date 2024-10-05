1f77fcb3ef859305a70ab10836407a4125ad3463
KR1JAKS <orlov.vlad204.83@gmail.com>
2024-10-05 14:44:41 +0700

    test2

---
+++ player/character_body_2d.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@export var speed = 300
+@export var accel = 10000
+@export var friction = 1500
+var screen_size
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("Right"):
+		velocity.x += 1
+	if Input.is_action_pressed("Left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("Down"):
+		velocity.y += 1
+	if Input.is_action_pressed("Up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	if velocity.x != 0 || velocity.y != 0:
+		$AnimatedSprite2D.animation = "right"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	
+func _ready():
+	screen_size = get_viewport_rect().size
