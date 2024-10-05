81428531232f87515ef18460601bfff09b857061
flexu5 <proboimax@gmail.com>
2024-10-05 19:53:26 +0700

    emmitmig
    emmitmig

--- player/character_body_2d.gd
+++ player/character_body_2d.gd
@@ -1,33 +1,45 @@
 extends CharacterBody2D
 
-@export var speed = 300
-@export var accel = 10000
-@export var friction = 1500
+const SPEED = 300.0
 var screen_size
 
-func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("Right"):
-		velocity.x += 1
-	if Input.is_action_pressed("Left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("Down"):
-		velocity.y += 1
-	if Input.is_action_pressed("Up"):
-		velocity.y -= 1
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
 
+func _physics_process(delta: float) -> void:
+	
+
+	# Получаем направление движения
+	var directionX := Input.get_axis("Left", "Right")
+	var directionY := Input.get_axis("Up", "Down")
+	
+	if directionX != 0:
+		velocity.x = directionX * SPEED
+	else:
+		velocity.x = 0
+
+	if directionY != 0:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = 0
+
+	# Перемещение с учетом столкновений
+	move_and_slide()
+
+	# Обновление анимации
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
 		$AnimatedSprite2D.animation = "idle"
+<<<<<<< Updated upstream
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+=======
+
+>>>>>>> Stashed changes
 	if velocity.x != 0 || velocity.y != 0:
 		$AnimatedSprite2D.animation = "right"
 		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	
-func _ready():
-	screen_size = get_viewport_rect().size
