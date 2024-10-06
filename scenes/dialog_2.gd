f091ef5d6b4f4f68929fdd9e0e057b1f0c8041a6
Владислав <orlov.vlad204.83@gmail.com>
2024-10-06 09:41:10 +0700

    12131313
    вывывывывывы

---
+++ scenes/dialog_2.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	entered = true
+
+
+func _on_body_exited(body) -> void:
+	if entered == true:
+		Dialogic.start("res://tl/ev3.dtl")
+		queue_free()
